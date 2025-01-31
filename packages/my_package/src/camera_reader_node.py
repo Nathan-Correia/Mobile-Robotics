f38a6a5fea19b29aece4eb7a81460a178bd6878b
nathan-correia <nathanc54@shaw.ca>
2025-01-31 16:50:53 -0700

    annotated-image

--- packages/my_package/src/camera_reader_node.py
+++ packages/my_package/src/camera_reader_node.py
@@ -3,7 +3,7 @@
 import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, Image
 
 import cv2
 from cv_bridge import CvBridge
@@ -23,13 +23,27 @@ class CameraReaderNode(DTROS):
         cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
         # construct subscriber
         self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
+        self._annotated_topic = f"/{self._vehicle_name}/camera_node/image/compressed/annotated"
+        self.pub = rospy.Publisher(self._annotated_topic, Image, queue_size=1)
 
     def callback(self, msg):
         # convert JPEG bytes to CV image
         image = self._bridge.compressed_imgmsg_to_cv2(msg)
-        # display frame
-        cv2.imshow(self._window, image)
-        cv2.waitKey(1)
+        height, width = image.shape[:2]
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        annotation_text = f"Duck {self._vehicle_name} says, 'Cheese! Capturing {width}X{height} - quack-tastic!'"
+        cv2.putText(
+            gray_image,
+            annotation_text,
+            (10, height - 10),  # position near the bottom-left corner
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,                # font scale
+            (255,),             # white color (for grayscale, a single value is enough)
+            2,                  # thickness
+            cv2.LINE_AA
+        )
+        annotated_msg = self._bridge.cv2_to_imgmsg(gray_image, encoding="mono8")
+        self.pub.publish(annotated_msg)
 
 if __name__ == '__main__':
     # create the node
